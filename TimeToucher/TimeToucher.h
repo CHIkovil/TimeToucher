b876e9202cf15a96f2e5dae41d0b35b8a5f64741
Nikolas <nikolas@Nikolass-MacBook-Air.local>
2021-08-25 23:08:30 +0300

    first commit

---
+++ TimeToucher/TimeToucher.h
@@ -0,0 +1,18 @@
+//
+//  TimeToucher.h
+//  TimeToucher
+//
+//  Created by Nikolas on 25.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TimeToucher.
+FOUNDATION_EXPORT double TimeToucherVersionNumber;
+
+//! Project version string for TimeToucher.
+FOUNDATION_EXPORT const unsigned char TimeToucherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeToucher/PublicHeader.h>
+
+
